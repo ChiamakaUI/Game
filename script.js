800f1d545c1ddf86097ed505a30dadf7b3bd53ef
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2020-05-09 14:55:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     scoreBoard.textContent = score;
 
+    next.addEventListener('click', ()=>{
+        
+    })
+
     section.forEach((section)=>{
         var form = section.children[1];
 
@@ -19,17 +23,38 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     if(form.elements[i].value == 'm'){
                         form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#31da4d';
                         form.elements[i].nextSibling.nextSibling.style.color = 'white';
+                        score = score + 1;
+                        scoreBoard.textContent = score;
+                        next.setAttribute('disabled', 'false');
 
+                    } else if(form.elements[i].value == 'g') {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
 
-                        form.elements[i].nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.setAttribute('disabled', true)
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.color = 'white';
+                        next.setAttribute('disabled', 'false');
 
-                        console.log(form.elements[i].nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
-                        nextSibling.nextSibling.nextSibling.ne
+                    } else {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
 
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.color = 'white';
+                        next.setAttribute('disabled', 'false');
+                        
+                    }
+                } else if(form.elements[i].getAttribute('name') == '2') {
+                    if(form.elements[i].value == 'f'){
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
                         score = score + 1;
                         scoreBoard.textContent = score;
 
-                    } else if(form.elements[i].value == 'g') {
+                    } else if(form.elements[i].value == 'l') {
                         form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
                         form.elements[i].nextSibling.nextSibling.style.color = 'white';
 
@@ -47,8 +72,81 @@ document.addEventListener('DOMContentLoaded', ()=>{
                         previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.color = 'white';
                         
                     }
-                } else if(form.elements[i].getAttribute('name') == '2') {
+                } else if(form.elements[i].getAttribute('name') == '3'){
+                    if(form.elements[i].value == '6'){
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+                        score = score + 1;
+                        scoreBoard.textContent = score;
 
+                    } else if(form.elements[i].value == 'l0') {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.color = 'white';
+
+                    } else {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.color = 'white';
+                        
+                    }
+                } else if(form.elements[i].getAttribute('name') == '4'){
+                    if(form.elements[i].value == 'm'){
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+                        score = score + 1;
+                        scoreBoard.textContent = score;
+
+                    } else if(form.elements[i].value == 'r') {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.color = 'white';
+
+                    } else {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.color = 'white';
+                        
+                    }
+                } else {
+                    if(form.elements[i].value == 'u'){
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+                        score = score + 1;
+                        scoreBoard.textContent = score;
+
+                    } else if(form.elements[i].value == 'e') {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+                        form.elements[i].previousSibling.previousSibling.previousSibling.style.color = 'white';
+
+                    } else {
+                        form.elements[i].nextSibling.nextSibling.style.backgroundColor = '#e21313';
+                        form.elements[i].nextSibling.nextSibling.style.color = 'white';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.backgroundColor = '#31da4d';
+
+                        form.elements[i].previousSibling.previousSibling.previousSibling.
+                        previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.style.color = 'white';
+                        
+                    }
                 }
             })
         }
